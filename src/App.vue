4bdb20b329566000bbc39c00115e176cbde0b255
Leonel Mandarino <leonelmandarino@gmail.com>
2022-05-25 11:38:31 -0300

    Add vuestic and improved UI

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,15 @@ const word_filters = ref({
   grey_letters: "",
 });
 
+function sliceIntoChunks(arr, chunkSize) {
+  const res = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+    const chunk = arr.slice(i, i + chunkSize);
+    res.push(chunk);
+  }
+  return res;
+}
+
 function handleReset() {
   m_words.value = total_words;
 }
@@ -35,56 +44,94 @@ function handleSubmit() {
 </script>
 
 <template>
-  <h1>Wordle helper!</h1>
+  <h1 class="display-1">Wordle helper!</h1>
 
-  <div>
-    <h3>Green letters:</h3>
-    <input
-      v-for="n in 5"
-      :key="n"
-      v-model.trim="word_filters.green_letters[n - 1]"
-      maxlength="1"
-      size="1"
-      placeholder="_"
-    />
-  </div>
-  <div>
-    <h3>Yellow letters:</h3>
-    <input
-      v-for="n in 5"
-      :key="n"
-      v-model.trim="word_filters.yellow_letters[n - 1]"
-      maxlength="1"
-      size="1"
-      placeholder="_"
-    />
-  </div>
-  <div>
-    <h3>Grey letters:</h3>
-    <input v-model.trim="word_filters.grey_letters" />
-  </div>
+  <form>
+    <div class="upper-form">
+      <div>
+        <h3 class="title">Green letters:</h3>
+        <div class="input-wrapper">
+          <va-input
+            v-for="n in 5"
+            :key="n"
+            v-model.trim="word_filters.green_letters[n - 1]"
+            maxlength="1"
+            placeholder="_"
+            outline
+            color="success"
+          />
+        </div>
+      </div>
 
-  <div>
-    <button @click="handleReset">Reset</button>
-    <button @click="handleSubmit">Save</button>
-  </div>
+      <div>
+        <h3 class="title">Yellow letters:</h3>
+        <div class="input-wrapper">
+          <va-input
+            v-for="n in 5"
+            :key="n"
+            v-model.trim="word_filters.yellow_letters[n - 1]"
+            maxlength="1"
+            placeholder="_"
+            outline
+            color="warning"
+          />
+        </div>
+      </div>
+    </div>
+    <div>
+      <h3 class="title">Grey letters:</h3>
+      <va-input v-model.trim="word_filters.grey_letters" outline color="gray" />
+    </div>
 
-  <div>
-    <h3>Possible words: {{ m_words.length }}</h3>
+    <div class="btn-wrapper">
+      <va-button
+        icon="restart_alt"
+        color="info"
+        gradient
+        :rounded="false"
+        @click="handleReset"
+        >Reset</va-button
+      >
+      <va-button
+        icon="check_circle"
+        color="success"
+        gradient
+        :rounded="false"
+        @click="handleSubmit"
+        >Save</va-button
+      >
+    </div>
+  </form>
+
+  <div v-if="m_words.length" class="words-container">
+    <h3 class="display-6">Possible words: {{ m_words.length }}</h3>
     <RecycleScroller
       class="scroller"
-      :items="m_words"
-      :item-size="30"
+      :items="sliceIntoChunks(m_words, 3)"
+      :item-size="45"
+      :keyField="null"
       page-mode
       v-slot="{ item }"
     >
-      {{ item }}
+      <va-button
+        class="word-option"
+        :rounded="false"
+        v-for="(word, index) in item"
+        :key="index"
+      >
+        {{ word }}
+      </va-button>
     </RecycleScroller>
+  </div>
+  <div v-else class="loading-div">
+    <va-progress-circle indeterminate />
   </div>
 </template>
 
 <style>
 @import "./assets/base.css";
+@import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;1,700&display=swap");
+@import url("https://fonts.googleapis.com/icon?family=Material+Icons");
 
 #app {
   max-width: 1280px;
@@ -94,11 +141,52 @@ function handleSubmit() {
   font-weight: normal;
 }
 
-h1 {
+h1.display-1 {
   text-align: center;
+  margin-bottom: 50px;
 }
 
-h3 {
-  margin-top: 50px;
+form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+form > div {
+  margin: 10px 0;
+}
+
+.input-wrapper {
+  display: flex;
+  max-width: 250px;
+}
+
+.upper-form {
+  display: flex;
+}
+
+.upper-form > div {
+  margin: 0 20px;
+}
+
+.btn-wrapper > button {
+  margin: 0 5px;
+}
+
+.loading-div {
+  margin: 50px 0;
+  display: flex;
+  justify-content: center;
+}
+
+.word-option {
+  margin: 5px !important;
+  width: 100px;
+}
+
+.words-container {
+  text-align: center;
+  margin: 50px 0px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+
 import { RecycleScroller } from "vue-virtual-scroller";
 import "vue-virtual-scroller/dist/vue-virtual-scroller.css";
 
+import { createVuestic } from "vuestic-ui";
+import "vuestic-ui/dist/vuestic-ui.css";
+
 const app = createApp(App);
-app.component("RecycleScroller", RecycleScroller).mount("#app");
+
+app.use(createVuestic());
+app.component("RecycleScroller", RecycleScroller);
+
+app.mount("#app");
